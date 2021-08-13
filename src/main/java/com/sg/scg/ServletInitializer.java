47e0041245b87fe74f0cf4576ca77fa8aaecaed1
sg-object <sg.object@gmail.com>
2021-08-13 09:41:26 +0900

    init

---
+++ src/main/java/com/sg/scg/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.sg.scg;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringCloudGatewayApplication.class);
+	}
+}
